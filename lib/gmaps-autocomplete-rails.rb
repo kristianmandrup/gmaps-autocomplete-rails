cc8302ef1cd9e4ff2d70e463f0d155c148a4de0b
Rinas <onerinas@gmail.com>
2016-12-31 13:03:23 +0530

    Made jquery ui files and gmaps-autocomplete css available to gem

--- lib/gmaps-autocomplete-rails.rb
+++ lib/gmaps-autocomplete-rails.rb
@@ -3,6 +3,9 @@ require "gmaps-autocomplete-rails/version"
 module GmapsAutoComplete
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'static_assets.load_static_assets' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      end
     end
   end
 end
